c33e44edc6aebfb9588823f8b708d80096487937
Cipher-Coder <matthew.rebehn@gmail.com>
2019-09-03 03:18:09 -0400

    added css resets

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,12 @@
 <script>
-	let name = "Aiden and Zoie's Homework Website";
+  let name = "Aiden and Zoie's Homework App";
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+  h1 {
+    color: #000000;
+    text-align: center;
+  }
 </style>
 
 <h1>{name}!</h1>
